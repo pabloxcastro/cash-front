7e537900a763bcd6d1d785bbe24acd0df04f5ddd
pabloxcastro <pablo.castro@hotmail.com>
2025-11-09 18:12:48 -0300

    change switch for route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Title from "./components/Title";
 import GlobalStyle from "./styles/global";
 import App from "./pages/App";
@@ -11,11 +11,11 @@ const Pagina404 = () => <div>Página 404</div>;
 ReactDOM.render(
   <BrowserRouter>
     <Title />
-    <Switch>
+    <Routes>
       <Route path="/" component={App} exact />
       <Route path="/add" component={AddTranscation} />
       <Route component={Pagina404} />
-    </Switch>
+    </Routes>
     <GlobalStyle />
   </BrowserRouter>,
   document.getElementById("root")
